31a725149c2ee0967ca972d89b81dbe66db72ee3
Jesseliz <choque101108@gmail.com>
2019-08-01 11:02:31 -0500

    reduciendo la funcion showAllMovieSerie

--- src/data.js
+++ src/data.js
@@ -1,17 +1,13 @@
 const app= {
-movieData: (url) =>{
+showAllMovieSerie: (url, arrayData) =>{
   const objPelis=[];
-  const arrayPelis =['American Pie', 'The Hangover', 'The Mask', 'Scary Movie', 'Deadpool', 'Back to the Future', 'Jurassic Park', 'The Lord of the Rings', 'Harry Potter', 'Avengers', 'Pulp Fiction', 'Die Hard', 'Twelve Monkeys', 'Man on Fire', 'Kill Bill', 'The Dark Knight', 'Metropolis', 'Inception', 'A Clockwork Orange', '2001: A Space Odyssey', 'The Exorcist', ' The Shining', 'The Omen', 'Hostel', 'It',
-  'Get out','Psycho','The babadook','The cabinet of Dr. Caligari','The silence of the Lambs','Girls Trip','Zoolander','I Heart Huckabees','Tenacious D in The Pick of Destiny','Bridesmaids','Indiana Jones','Star Wars','Jumanji','Jurassic Park','Pirates of the Caribbean: The Curse of the Black Pearl','Mad Max: Fury Road','Metropolis','Gravity','War for the Planet of the Apes','Wall-E','Die Hard','Terminator 2: Judgment Day','The Dark Knight','Drive', 'E.T. The Extra-Terrestrial', 'Seven Samurai'];
-  //const arrayPelis =['Get out','Psycho','The babadook','The cabinet of Dr. Caligari',];
-    for (let i=0; i< arrayPelis.length; i++)
+    for (let i=0; i< arrayData.length; i++)
     {   
-       let url_pelis = url+arrayPelis[i]+'&apikey=4c2bc917'
+       let url_pelis = url+arrayData[i]+'&apikey=4c2bc917'
        fetch(url_pelis ) 
       .then(response => response.json())
       .then(data => {
         localStorage.setItem('data', JSON.stringify(data))
-        // console.log (data); 
         objPelis.push(data);
         createTemplateCard(objPelis)
        return objPelis;   
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 const movieContainer = document.getElementById('all-movies');
 const urlMovie = 'https://www.omdbapi.com/?t=';
 const urlSerie = 'https://www.omdbapi.com/?s=';
-app.movieData(urlMovie);
+const arrayPelis =['American Pie', 'The Hangover', 'The Mask', 'Scary Movie', 'Deadpool', 'Back to the Future', 'Jurassic Park', 'The Lord of the Rings', 'Harry Potter', 'Avengers', 'Pulp Fiction', 'Die Hard', 'Twelve Monkeys', 'Man on Fire', 'Kill Bill', 'The Dark Knight', 'Metropolis', 'Inception', 'A Clockwork Orange', '2001: A Space Odyssey', 'The Exorcist', ' The Shining', 'The Omen', 'Hostel', 'It',
+  'Get out','Psycho','The babadook','The cabinet of Dr. Caligari','The silence of the Lambs','Girls Trip','Zoolander','I Heart Huckabees','Tenacious D in The Pick of Destiny','Bridesmaids','Indiana Jones','Star Wars','Jumanji','Jurassic Park','Pirates of the Caribbean: The Curse of the Black Pearl','Mad Max: Fury Road','Metropolis','Gravity','War for the Planet of the Apes','Wall-E','Die Hard','Terminator 2: Judgment Day','The Dark Knight','Drive', 'E.T. The Extra-Terrestrial', 'Seven Samurai'];
+
+	app.showAllMovieSerie(urlMovie,arrayPelis);
+
 const createTemplateCard = list => {
   let templateCard = '';
 	list.forEach(element => {
