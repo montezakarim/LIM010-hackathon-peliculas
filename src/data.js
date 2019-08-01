4222f91bceeb458027bd17ad5cb4f2df1a7347e0
montezakarim <kmonteza@hotmail.com>
2019-08-01 11:05:21 -0500

    buscar

--- src/data.js
+++ src/data.js
@@ -4,6 +4,12 @@ movieData: () =>{
 },
 };
 window.app = app;
+
 console.log(app.movieData);
 
 
+const searchMovie = (data,peli) => {
+    const results = data.filter(obj => obj.title.toLowerCase().startsWith(peli));
+    return results;
+};
+window.searchMovie = searchMovie;
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+const inputSearch = document.getElementById('input-search');
+const submitSearch = document.getElementById('submitSearch');
 const objPelis=[];
   const arrayPelis =['American Pie', 'The Hangover', 'The Mask', 'Scary Movie', 'Deadpool', 'Back to the Future', 'Jurassic Park', 'The Lord of the Rings', 'Harry Potter', 'Avengers', 'Pulp Fiction', 'Die Hard', 'Twelve Monkeys', 'Man on Fire', 'Kill Bill', 'The Dark Knight', 'Metropolis', 'Inception', 'A Clockwork Orange', '2001: A Space Odyssey', 'The Exorcist', ' The Shining', 'The Omen', 'Hostel', 'It',
   'Get out','Psycho','The babadook','The cabinet of Dr. Caligari','The silence of the Lambs','Girls Trip','Zoolander','I Heart Huckabees','Tenacious D in The Pick of Destiny','Bridesmaids','Indiana Jones','Star Wars','Jumanji','Jurassic Park','Pirates of the Caribbean: The Curse of the Black Pearl','Mad Max: Fury Road','Metropolis','Gravity','War for the Planet of the Apes','Wall-E','Die Hard','Terminator 2: Judgment Day','The Dark Knight','Drive', 'E.T. The Extra-Terrestrial', 'Seven Samurai'];
@@ -14,7 +16,7 @@ const objPelis=[];
         objPelis.push(data);
         createTemplateCard(objPelis)
        return objPelis;   
-      })   
+      })
       .catch(err => (err))
     }
 const movieContainer = document.getElementById('all-movies');
@@ -36,3 +38,9 @@ const createTemplateCard = list => {
 	});
 	movieContainer.innerHTML = templateCard;
 };
+
+// Buscar peli
+inputSearch.addEventListener('input', event => {
+  const searchMovies = searchMovie(objPelis, event.target.value.toLowerCase());
+  movieContainer.innerHTML = templateCard(searchMovies);
+});
