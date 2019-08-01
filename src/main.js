157bc83589999d1728c65631a46a12f3d08e365a
Betsycaceres <betsy0357@gmail.com>
2019-08-01 13:06:55 -0500

    agregando series al arraySeries

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const urlMovie = 'https://www.omdbapi.com/?t=';
 const urlSerie = 'http://www.omdbapi.com/?s=';
 const arrayPelis =['American Pie', 'The Hangover', 'The Mask', 'Scary Movie', 'Deadpool', 'Back to the Future', 'Jurassic Park', 'The Lord of the Rings', 'Harry Potter', 'Avengers', 'Pulp Fiction', 'Die Hard', 'Twelve Monkeys', 'Man on Fire', 'Kill Bill', 'The Dark Knight', 'Metropolis', 'Inception', 'A Clockwork Orange', '2001: A Space Odyssey', 'The Exorcist', ' The Shining', 'The Omen', 'Hostel', 'It',
   'Get out','Psycho','The babadook','The cabinet of Dr. Caligari','The silence of the Lambs','Girls Trip','Zoolander','I Heart Huckabees','Tenacious D in The Pick of Destiny','Bridesmaids','Indiana Jones','Star Wars','Jumanji','Jurassic Park','Pirates of the Caribbean: The Curse of the Black Pearl','Mad Max: Fury Road','Metropolis','Gravity','War for the Planet of the Apes','Wall-E','Die Hard','Terminator 2: Judgment Day','The Dark Knight','Drive', 'E.T. The Extra-Terrestrial', 'Seven Samurai'];
-const arraySeries =['Sex Education', 'Pocoyo and the Space Circus','Friends', 'Suits', 'The Flash', 'Dark', 'Breaking Bad', 'Zoo','Black Mirror: Bandersnatch', 'Merlí','Mickey Mouse Clubhouse', 'The Rain', 'Sense8','Blindspot','The OA'];
+const arraySeries =['Sex Education','Glee','Outlander','Pocoyo and the Space Circus','Friends','The Sinner','Suits', 'The Flash', 'Dark', 'Breaking Bad', 'Zoo','Black Mirror: Bandersnatch', 'Merlí','Mickey Mouse Clubhouse', 'The Rain', 'Sense8','Blindspot','The OA'];
 // app.showAllMovieSerie(urlMovie,arrayPelis);
 app.showAllMovieSerie(urlSerie,arraySeries);
 // const createTemplateCard = list => {
